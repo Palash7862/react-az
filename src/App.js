8436864931bd7385a1ca87abe71269d748e233cb
MD. Samsozzaman Palash <palashkhan.pk@gmail.com>
2018-06-10 04:39:07 +0600

    working with react component children

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Does this Working?</h1>
         <Person name="Palash"/>
+        <Person name="Palash">This is Child </Person>
       </div>
     );
     // var childEl = React.createElement('h1', null, 'Does this Working?');
