7114b7d34f051745706c0d65ae7958433c27e2eb
MD. Samsozzaman Palash <palashkhan.pk@gmail.com>
2018-06-10 05:08:25 +0600

    working with react state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      {name: 'Nikname 1', age: 22}, 
+      {name: 'Nikname 2', age: 23} 
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Does this Working?</h1>
         <Person name="Palash"/>
         <Person name="Palash">This is Child </Person>
+        <h1>State Test</h1>
+        <h5>My {this.state.person[0].name} and age {this.state.person[0].age}</h5>
+        <h5>My {this.state.person[1].name} and age {this.state.person[1].age}</h5>
       </div>
     );
     // var childEl = React.createElement('h1', null, 'Does this Working?');
